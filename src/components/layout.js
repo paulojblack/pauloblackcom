a62da4e00f5c1d2248c82e0d980500d1d73c8079
Paulo Black <paulo@pauloblack.com>
2023-06-11 06:41:34 -0400

    rm abortive goodreads widget code

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,19 +30,6 @@ const Layout = ({ children }) => {
     }
   `)
 
-  React.useEffect(() => {
-    const script = document.createElement('script');
-
-    script.src = "https://www.goodreads.com/review/custom_widget/45979099.Currently%20Reading?cover_position=left&cover_size=small&num_books=5&order=a&shelf=currently-reading&show_author=1&show_cover=1&show_rating=0&show_review=1&show_tags=0&show_title=1&sort=date_added&widget_bg_color=FFFFFF&widget_bg_transparent=&widget_border_width=1&widget_id=1686392425&widget_text_color=000000&widget_title_size=medium&widget_width=medium";
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    }
-  }, []);
-
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Goodreads from "../components/goodreads"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
